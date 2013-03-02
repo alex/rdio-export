1625df51e42adaae1e82dd30b66bf6e6f7620e6c
Alex Gaynor <alex.gaynor@rd.io>
2013-03-01 21:44:21 -0800

    Give a userful warning.

--- static/js/site/site.js
+++ static/js/site/site.js
@@ -8,6 +8,10 @@ App.utils = {
 Raven.config("https://1c57a3e6b639450cafc418709c7e9a54@app.getsentry.com/5995").install();
 
 $(function() {
+    if (!window.URL || !window.URL.createObjectURL) {
+        alert("Sorry, this requires a browser which supports " +
+            "window.URL.createObjectURL (see https://developer.mozilla.org/en-US/docs/DOM/window.URL.createObjectURL)");
+    }
     R.ready(function() {
         plate.Template.Meta.registerFilter("get", function(obj, fieldName) {
             return obj.get(fieldName);
