0af8585fde3bd521d7cd9b7f1e8473020d71102a
Alex Gaynor <alex.gaynor@rd.io>
2013-03-01 18:02:36 -0800

    record errors

--- static/js/site/site.js
+++ static/js/site/site.js
@@ -5,6 +5,8 @@ App.utils = {
     }
 };
 
+Raven.config("https://1c57a3e6b639450cafc418709c7e9a54@app.getsentry.com/5995").install();
+
 $(function() {
     R.ready(function() {
         plate.Template.Meta.registerFilter("get", function(obj, fieldName) {
